5b19fed239ef92eab7d9c6e97e302fafb0a1fb23
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-03-20 11:13:43 +0900

    Initial Commit

---
+++ Sample22-LongPress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample22-LongPress
+//
+//  Created by keiji yamaki on 2021/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample22-LongPress/Sample22_LongPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample22_LongPressApp.swift
+//  Sample22-LongPress
+//
+//  Created by keiji yamaki on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct Sample22_LongPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
